550832c4e0b3aad37d3b1bea2bfc5e5974671be3
kwakyongwoo <kkolbuyw@gmail.com>
2025-02-27 13:34:12 +0900

    add styled-components preload

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import StyledComponentsRegistry from './lib/registry'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +21,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="kr">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <StyledComponentsRegistry>
         {children}
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
